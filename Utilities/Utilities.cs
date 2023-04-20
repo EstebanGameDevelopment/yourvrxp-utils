b01a95a1b8a82a43ee8815e7f03d8cd795e00378
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-04-20 11:29:56 +0200

    Method to shuffle a list

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -311,5 +311,18 @@ namespace yourvrexperience.Utils
             }
             return output;
         }
+
+		public static void Shuffle<T>(this IList<T> list, System.Random rnd)
+		{
+			for(var i=list.Count; i > 0; i--)
+				list.Swap(0, rnd.Next(0, i));
+		}
+
+		public static void Swap<T>(this IList<T> list, int i, int j)
+		{
+			var temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
 	}
 }
