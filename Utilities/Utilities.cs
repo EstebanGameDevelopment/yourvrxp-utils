499dea5a3cbfbfd4daf7964281f66032c0bb78d1
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-05-02 17:23:58 +0200

    Functionality used for random code generation.

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -388,5 +388,20 @@ namespace yourvrexperience.Utils
 			Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_camera);
 			return GeometryUtility.TestPlanesAABB(planes, _bounds);
 		}
+
+		public static string RandomCodeIV(int _size)
+		{
+			string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#!@+=-*";
+			var stringChars = new char[_size];
+			var random = new System.Random();
+
+			for (int i = 0; i < _size; i++)
+			{
+				stringChars[i] = chars[random.Next(chars.Length)];
+			}
+
+			string finalString = new String(stringChars);
+			return finalString;
+		}
 	}
 }
