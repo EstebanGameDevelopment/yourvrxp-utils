fc05140400ebdc58e2fda00bb4c719be317d9276
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-05-12 16:34:37 +0200

    Local listener of UI events for the BaseScreenView

--- Controllers/Screens/Components/BaseScreenView.cs
+++ Controllers/Screens/Components/BaseScreenView.cs
@@ -13,6 +13,7 @@ namespace yourvrexperience.Utils
     {
 		public const string EventBaseScreenViewCreated = "EventBaseScreenViewCreated";
 		public const string EventBaseScreenViewDestroyed = "EventBaseScreenViewDestroyed";
+		public const string EventBaseScreenViewEnableInteraction = "EventBaseScreenViewEnableInteraction";
 
 		protected Transform _content;
 		protected Transform _background;
@@ -38,6 +39,7 @@ namespace yourvrexperience.Utils
 
 			Debug.Assert(_content != null);
 
+			UIEventController.Instance.Event += OnUILocalEvent;
 			SystemEventController.Instance.DispatchSystemEvent(EventBaseScreenViewCreated, this.gameObject);
 #if ENABLE_OCULUS || ENABLE_OPENXR || ENABLE_ULTIMATEXR
 			if (VRInputController.Instance != null) VRInputController.Instance.DispatchVREvent(VRInputController.EventVRInputControllerResetAllInputs);
@@ -46,10 +48,20 @@ namespace yourvrexperience.Utils
 
 		public virtual void Destroy()
 		{
+			if (UIEventController.Instance != null) UIEventController.Instance.Event -= OnUILocalEvent;
 			SystemEventController.Instance.DelaySystemEvent(EventBaseScreenViewDestroyed, 0.1f);	
 #if ENABLE_OCULUS || ENABLE_OPENXR || ENABLE_ULTIMATEXR
 			if (VRInputController.Instance != null) VRInputController.Instance.DispatchVREvent(VRInputController.EventVRInputControllerResetAllInputs);
 #endif			
 		}
-	}
+
+        private void OnUILocalEvent(string nameEvent, object[] parameters)
+        {
+            if (nameEvent.Equals(EventBaseScreenViewEnableInteraction))
+			{				
+				bool interactivity = (bool)parameters[0];
+				Utilities.ApplyEnabledInteraction(this.gameObject.transform, interactivity);
+			}
+        }
+    }
 }
