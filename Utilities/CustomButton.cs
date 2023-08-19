9afc6b5a4c6b890275451c35a53572dc0c40026c
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-08-19 19:27:32 +0200

    Call the base method to respect Button behavior

--- Utilities/CustomButton.cs
+++ Utilities/CustomButton.cs
@@ -17,18 +17,22 @@ namespace yourvrexperience.Utils
 
 		public override void OnPointerClick(PointerEventData eventData)
 		{
+			base.OnPointerClick(eventData);
 			ClickedButton?.Invoke(this);
 		}
 		public override void OnPointerDown(PointerEventData eventData)
 		{
+			base.OnPointerDown(eventData);
 			PointerDownButton?.Invoke(this);
 		}
         public override void OnPointerEnter(PointerEventData eventData)
 		{
+			base.OnPointerEnter(eventData);
 			PointerEnterButton?.Invoke(this);
 		}
         public override void OnPointerExit(PointerEventData eventData)
 		{
+			base.OnPointerExit(eventData);
 			PointerExitButton?.Invoke(this);
 		}
         public override void OnPointerUp(PointerEventData eventData)
