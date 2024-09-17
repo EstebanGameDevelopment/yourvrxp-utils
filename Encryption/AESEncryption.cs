dcf32e7448ab2bce5b9bb5e6e7543125451af797
EstebanGameDevelopment <services@yourvrexperience.com>
2024-09-17 17:45:41 +0200

    Extended encryption functionalities

--- Encryption/AESEncryption.cs
+++ Encryption/AESEncryption.cs
@@ -12,45 +12,38 @@ namespace yourvrexperience.Utils
 		public struct AESEncryptedText
 		{
 			public string IV;
-			public string EncryptedText;
+			public byte[] EncryptedData;
 		}
 
-		public static AESEncryptedText Encrypt(string plainText, string password)
+		public static AESEncryptedText Encrypt(byte[] plainData, string password)
 		{
 			using (var aes = Aes.Create())
 			{
 				aes.GenerateIV();
 				aes.Key = ConvertToKeyBytes(aes, password);
 
-				var textBytes = Encoding.UTF8.GetBytes(plainText);
-
 				var aesEncryptor = aes.CreateEncryptor();
-				var encryptedBytes = aesEncryptor.TransformFinalBlock(textBytes, 0, textBytes.Length);
+				var encryptedBytes = aesEncryptor.TransformFinalBlock(plainData, 0, plainData.Length);
 
 				return new AESEncryptedText
 				{
 					IV = Convert.ToBase64String(aes.IV),
-					EncryptedText = Convert.ToBase64String(encryptedBytes)
+					EncryptedData = encryptedBytes
 				};
 			}
 		}
 
-		public static string Decrypt(AESEncryptedText encryptedText, string password)
-		{
-			return Decrypt(encryptedText.EncryptedText, encryptedText.IV, password);
-		}
-
-		public static string Decrypt(string encryptedText, string iv, string password)
+		public static byte[] Decrypt(byte[] encryptedData, string iv, string password)
 		{
 			using (Aes aes = Aes.Create())
 			{
 				var ivBytes = Convert.FromBase64String(iv);
-				var encryptedTextBytes = Convert.FromBase64String(encryptedText);
+				var encryptedTextBytes = encryptedData;
 
 				var decryptor = aes.CreateDecryptor(ConvertToKeyBytes(aes, password), ivBytes);
 				var decryptedBytes = decryptor.TransformFinalBlock(encryptedTextBytes, 0, encryptedTextBytes.Length);
 
-				return Encoding.UTF8.GetString(decryptedBytes);
+				return decryptedBytes;
 			}
 		}
 
@@ -69,6 +62,58 @@ namespace yourvrexperience.Utils
 			}
 
 			return keyBytes;
+		}
+
+		// This method will encrypt the byte array using AES.
+		public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+		{
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+				using (var memoryStream = new MemoryStream())
+				{
+					using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+					{
+						cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+						cryptoStream.FlushFinalBlock();
+						return memoryStream.ToArray();
+					}
+				}
+			}
+		}
+
+		// This method will decrypt the byte array back to its original form.
+		public static byte[] Decrypt(byte[] encryptedData, byte[] key, byte[] iv)
+		{
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+				using (var memoryStream = new MemoryStream(encryptedData))
+				using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+				using (var resultStream = new MemoryStream())
+				{
+					cryptoStream.CopyTo(resultStream);
+					return resultStream.ToArray();
+				}
+			}
+		}
+
+		// Generate a random key and IV (for demonstration purposes).
+		public static void GenerateKeyAndIV(out byte[] key, out byte[] iv)
+		{
+			using (Aes aes = Aes.Create())
+			{
+				aes.GenerateKey();
+				aes.GenerateIV();
+				key = aes.Key;
+				iv = aes.IV;
+			}
 		}
 	}
 }
