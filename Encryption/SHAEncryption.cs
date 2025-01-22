d1f12761debfa541a20285949414f44435f71cb0
EstebanGameDevelopment <services@yourvrexperience.com>
2025-01-22 13:03:40 +0100

    Encrytpion methods with Salt

--- Encryption/SHAEncryption.cs
+++ Encryption/SHAEncryption.cs
@@ -18,5 +18,39 @@ namespace yourvrexperience.Utils
                 return Convert.ToBase64String(hash);
             }
         }
+
+        public static string GenerateSalt()
+        {
+            
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string GenerateSaltWithTimestamp(string salt, long timestamp)
+        {
+            // Get the current timestamp
+            string saltWithTimestamp = $"{salt}{timestamp}";
+
+            // Hash the salt and timestamp together
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltWithTimestamp));
+                return Convert.ToBase64String(hashBytes); // Opaque combined value
+            }
+        }
+
+        public static string HashWithSalt(string input, string combinedSalt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] combinedBytes = Encoding.UTF8.GetBytes(input + combinedSalt);
+                byte[] hashBytes = sha256.ComputeHash(combinedBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
     }
 }
