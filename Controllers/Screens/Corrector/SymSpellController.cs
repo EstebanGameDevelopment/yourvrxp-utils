1cfe8ec58fb6b25105e31be3cda70b9508ce9a15
EstebanGameDevelopment <services@yourvrexperience.com>
2025-06-09 15:49:48 +0200

    Removed the reference to the specific project

--- Controllers/Screens/Corrector/SymSpellController.cs
+++ Controllers/Screens/Corrector/SymSpellController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using yourvrexperience.aibookeditor;
 using static SymSpell;
 
 namespace yourvrexperience.Utils
